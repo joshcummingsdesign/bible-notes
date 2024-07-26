d4ae93c935f7ce765706f4f32150f008781fc1f4
Josh Cummings <joshcummingsdesign@gmail.com>
2024-07-26 03:03:32 -0700

    update wp

--- wp-content/plugins/kadence-blocks/vendor/composer/installed.php
+++ wp-content/plugins/kadence-blocks/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'kadencewp/kadence-blocks',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '25faf21e5fc664a6710bbba7bf48dde308ffc36b',
+        'reference' => '92272ec506402dd02e07aa692a7c6d68ef81b3e4',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -40,7 +40,7 @@
         'kadencewp/kadence-blocks' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '25faf21e5fc664a6710bbba7bf48dde308ffc36b',
+            'reference' => '92272ec506402dd02e07aa692a7c6d68ef81b3e4',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
